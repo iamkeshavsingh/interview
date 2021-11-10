7ad5d604ef5a77a999e898a09bcf405bdadf8c04
ksingh_itp <ksingh.itp@sparkcognition.com>
2021-11-10 06:27:35 +0530

    persist added

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,28 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const persistConfig = {
+  key: 'root',
+  storage,
+  blackList: ['token']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+const persistor = persistStore(store)
 
 function StoreProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>{children}</PersistGate>
+    </Provider>
+  );
 }
 
 export default StoreProvider;
